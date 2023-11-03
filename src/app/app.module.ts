e9e9c3686c2fd50566fce450da1825413503f481
Daniel Haida <danilgajda22@gmail.com>
2023-11-03 10:30:22 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const MaterialComponents = [];
 @NgModule({
-  declarations: [AppComponent, ProductComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductListComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatCardModule,
+    MatToolbarModule,
+    MatTabsModule,
     BrowserAnimationsModule,
   ],
   providers: [],
